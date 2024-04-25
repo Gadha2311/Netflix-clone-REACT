7104ce0d7d86354b83acb2fb05822561ba1a0c35
gadha venu <gadha2311@gmail.com>
2024-04-25 11:58:40 -0500

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./components/navbar/navbar";
+import Banner from "./components/Banner/Banner";
+import Rowpost from "./components/Rowpost/Rowpost";
+import { action, comedy, documentaries, horror, originals, romance } from "./urls";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Rowpost url={originals} title="Netflix Originals" />
+      <Rowpost url={action} title="Action" isSmall />
+      <Rowpost url={comedy} title="Comedy" isSmall />
+      <Rowpost url={horror} title="Horror" isSmall />
+      <Rowpost url={romance} title="Romance" isSmall />
+      <Rowpost url={documentaries} title="Documentaries" isSmall />
     </div>
   );
 }
